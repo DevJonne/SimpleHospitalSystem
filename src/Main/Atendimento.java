b6ffae702335f8769f1f66d9ecd9c86e2e8a4de9
JonnS3 <jonathaninabraga@gmail.com>
2024-06-19 19:32:47 -0300

    v1

---
+++ src/Main/Atendimento.java
@@ -0,0 +1,25 @@
+package Main;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class Atendimento {
+private LocalDate data;
+private String descricao;
+
+public String getDescricao() {
+return descricao;
+}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+        @Override
+        public String toString(){
+            DateTimeFormatter formatoBr = DateTimeFormatter.
+            ofPattern("dd/MM/yyyy");
+                String data = formatoBr.format(this.data);
+                String retorno = "Data: "+data;
+                retorno += "InInformações: "+descricao;
+            return retorno;
+        }
+}
